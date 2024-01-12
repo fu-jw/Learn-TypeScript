eeac76907dcb6127e1b6f0d28a391f54c3e76323
fu-jw <contact@fu-jw.com>
2024-01-12 20:39:07 +0800

    add：01.string类型

---
+++ 01.string类型.ts
@@ -0,0 +1,11 @@
+// 可以用单引号或双引号表示字符串
+let str1:string = 'Hello';
+let str2 = "TypeScript";
+
+// 可以字符串拼接
+let str3 = str1 + str2;
+console.log(str3);// HelloTypeScript
+
+// 支持ES6的模板字符串来拼接变量和字符串
+str3 = `你好 ${str2}`;
+console.log(str3);// 你好 TypeScript

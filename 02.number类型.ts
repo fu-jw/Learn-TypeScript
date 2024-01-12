e3cbe73de5ba8dd95a57934c3dc347fe8a5319f4
fu-jw <contact@fu-jw.com>
2024-01-12 20:43:31 +0800

    add：02.number类型

---
+++ 02.number类型.ts
@@ -0,0 +1,8 @@
+// 不区分整数和浮点数，都是number类型
+let num = 123;
+num = 45.6;
+
+// ES6新增了二进制和八进制的表示方法，而TypeScript也是支持二进制、八进制、十六进制
+let binaryNum:number = 0b1111;
+let octalNum:number = 0o123;
+let hexNum:number = 0x123;

6165192c99daa19a0cf4a59c12aae54978213c65
fu-jw <contact@fu-jw.com>
2024-01-12 20:28:42 +0800

    add : 00.hello-ts

---
+++ 00.hello-ts.ts
@@ -0,0 +1,8 @@
+let msg:string = 'Hello TypeScript';
+// msg = 123;// error: Type 'number' is not assignable to type 'string'
+console.log(msg);
+///////////////////////////////////////////////////////////////////////////
+let num = 123;// 可自动推断类型，let num: number
+// num = '123';// error: Type 'string' is not assignable to type 'number'
+//字面量类型
+const hight = 345;// 但const自动推断类型成，const hight: 345

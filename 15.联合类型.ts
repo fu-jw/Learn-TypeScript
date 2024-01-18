84dd0276ad2598aac804647375d4de977dd66cf1
fu-jw <contact@fu-jw.com>
2024-01-18 14:45:30 +0800

    add: 15.联合类型

---
+++ 15.联合类型.ts
@@ -0,0 +1,24 @@
+// 1.联合类型的基本使用
+let foo: number | string = "abc"
+foo = 123
+
+// 使用的时候要特别的小心
+if (typeof foo === "string") {
+  // console.log(foo.length)// error: Property 'length' does not exist on type 'never'.
+}
+
+
+// 2. 打印id
+function printID(id: number | string) {
+  console.log("您的ID:", id)
+
+  // 类型缩小
+  if (typeof id === "string") {
+    console.log(id.length)
+  } else {
+    console.log(id)
+  }
+}
+
+printID("abc")
+printID(123)

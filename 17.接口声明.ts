b0325320dc166fb445539dd44c457d4e3981c56b
fu-jw <contact@fu-jw.com>
2024-01-19 21:41:11 +0800

    add:17.接口声明

---
+++ 17.接口声明.ts
@@ -0,0 +1,25 @@
+const name = "fredo"
+
+console.log("fredo")
+console.log(name)
+
+type PointType = {
+  x: number
+  y: number
+  z?: number
+}
+
+// 接口: interface
+// 声明的方式
+interface PointType2 {
+  x: number
+  y: number
+  z?: number
+}
+
+function printCoordinate(point: PointType2) {
+  
+}
+
+
+export {}
